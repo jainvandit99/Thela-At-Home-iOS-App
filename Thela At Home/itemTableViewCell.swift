79a1b47ca8bf6e1c92790ad673e724650b4f9f56
jainvandit <mailvandit.j@gmail.com>
2020-04-13 19:51:32 +0530

    Added a lot of other screens

---
+++ Thela At Home/itemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  itemTableViewCell.swift
+//  Thela At Home
+//
+//  Created by Vandit Jain on 12/04/20.
+//  Copyright © 2020 jainvandit. All rights reserved.
+//
+
+import UIKit
+
+class itemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
